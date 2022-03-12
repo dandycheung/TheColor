394c9a483117616427049309b17390961f6f917e
ordogod <mmolosays@gmail.com>
2022-03-12 20:50:45 +0300

    Mark 'Feature*Util' objects as 'internal'

--- feature/home/src/main/java/com/ordolabs/feature_home/util/FeatureHomeUtil.kt
+++ feature/home/src/main/java/com/ordolabs/feature_home/util/FeatureHomeUtil.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import com.ordolabs.feature_home.di.FeatureHomeComponent
 import com.ordolabs.thecolor.util.ext.scopedComponentsManager
 
-object FeatureHomeUtil {
+internal object FeatureHomeUtil {
 
     val Fragment.featureHomeComponent: FeatureHomeComponent
         get() = scopedComponentsManager.componentOf()
--- feature/settings/src/main/java/com/ordolabs/feature_settings/util/FeatureSettingsUtil.kt
+++ feature/settings/src/main/java/com/ordolabs/feature_settings/util/FeatureSettingsUtil.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import com.ordolabs.feature_settings.di.FeatureSettingsComponent
 import com.ordolabs.thecolor.util.ext.scopedComponentsManager
 
-object FeatureSettingsUtil {
+internal object FeatureSettingsUtil {
 
     val Fragment.featureSettingsComponent: FeatureSettingsComponent
         get() = scopedComponentsManager.componentOf()
