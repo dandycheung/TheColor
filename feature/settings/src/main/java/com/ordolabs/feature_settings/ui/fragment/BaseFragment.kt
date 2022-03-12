05c0ad207cfa8b765820adcb5190f6c585760c30
ordogod <mmolosays@gmail.com>
2022-03-12 22:39:15 +0300

    Subscribe all settings fragments for component

--- feature/settings/src/main/java/com/ordolabs/feature_settings/ui/fragment/BaseFragment.kt
+++ feature/settings/src/main/java/com/ordolabs/feature_settings/ui/fragment/BaseFragment.kt
@@ -1,9 +1,16 @@
 package com.ordolabs.feature_settings.ui.fragment
 
+import android.content.Context
 import com.ordolabs.feature_settings.R
+import com.ordolabs.feature_settings.util.FeatureSettingsUtil.featureSettingsComponentStore
 import com.ordolabs.thecolor.ui.fragment.BaseFragment as AppBaseFragment
 
 abstract class BaseFragment : AppBaseFragment() {
 
     override val defaultFragmentContainerId: Int = R.id.defaultFragmentContainer
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        featureSettingsComponentStore?.subscribe(lifecycle)
+    }
 }
