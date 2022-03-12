6e56bcf51b7c57749498fb10dc38041d5ee4c341
ordogod <mmolosays@gmail.com>
2022-03-12 22:29:52 +0300

    Introduce 'Fragment.featureSettingsComponentStore' extension function

--- feature/settings/src/main/java/com/ordolabs/feature_settings/util/FeatureSettingsUtil.kt
+++ feature/settings/src/main/java/com/ordolabs/feature_settings/util/FeatureSettingsUtil.kt
@@ -3,9 +3,13 @@ package com.ordolabs.feature_settings.util
 import androidx.fragment.app.Fragment
 import com.ordolabs.feature_settings.di.FeatureSettingsComponent
 import com.ordolabs.thecolor.util.ext.scopedComponentsManager
+import com.ordolabs.thecolor.util.struct.scopedcomponent.ScopedComponentStore
 
 internal object FeatureSettingsUtil {
 
     val Fragment.featureSettingsComponent: FeatureSettingsComponent
         get() = scopedComponentsManager.componentOf()
+
+    val Fragment.featureSettingsComponentStore: ScopedComponentStore<*>?
+        get() = scopedComponentsManager.storeOf<FeatureSettingsComponent>()
 }
